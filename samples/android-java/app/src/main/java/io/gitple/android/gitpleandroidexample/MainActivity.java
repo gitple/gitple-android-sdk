5761e5f77ba314e0aab4f5ae8189d7fce704d21a
jaeyong <jaeyong.lim@gitple.com>
2018-02-12 14:10:10 +0900

    add gitple SDK for example project
    - gitple SDK v1.1.1

--- samples/android-java/app/src/main/java/io/gitple/android/gitpleandroidexample/MainActivity.java
+++ samples/android-java/app/src/main/java/io/gitple/android/gitpleandroidexample/MainActivity.java
@@ -5,11 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.andremion.counterfab.CounterFab;
+
+import io.gitple.android.sdk.Gitple;
+import io.gitple.android.sdk.GitpleCallback;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Gitple:MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +25,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        CounterFab counterFab = (CounterFab) findViewById(R.id.counter_fab);
+        counterFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // Launch gitple Activity
+                Gitple.launch(MainActivity.this);
             }
         });
     }
@@ -48,5 +55,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Get unread count at gitple
+        Gitple.unreadCount(new GitpleCallback<Number>() {
+            @Override
+            public void onSuccess(Number data) {
+                Log.v(TAG, "unreadCount Callback onSuccess. data=" + data);
+                CounterFab counterFab = (CounterFab) findViewById(R.id.counter_fab);
+                counterFab.setCount(data.intValue());
+            }
+
+            @Override
+            public void onError(Gitple.Error errorCode) {
+                Log.v(TAG, "unreadCount Callback onError. code=" + errorCode);
+            }
+        });
     }
 }
---
+++ samples/android-java/app/src/main/java/io/gitple/android/gitpleandroidexample/MyCustomApplication.java
@@ -0,0 +1,38 @@
+package io.gitple.android.gitpleandroidexample;
+
+import android.app.Application;
+import android.util.Log;
+
+import io.gitple.android.sdk.Gitple;
+
+public class MyCustomApplication extends Application {
+    // Called when the application is starting, before any other application objects have been created.
+    // Overriding this method is totally optional!
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize Gitple with appCode
+        Gitple.initialize(this, "0gB8bTBoPOE6lWnleDun9wRGIeef11");
+
+        // hide gitple header
+        Gitple.config().setHideHeader(true);
+
+        // gitple user login
+        loginUser();
+    }
+
+    public void loginUser() {
+        // Register gitple user
+        Gitple.user().setId("userid0000001")
+                .setEmail("android@gitple.com")
+                .setName("android user")
+                .setPhone("010-0000-0000")
+                .setMeta("company", "gitple");
+    }
+
+    public void logoutUser() {
+        // reset gitple user
+        Gitple.user().reset();
+    }
+}
